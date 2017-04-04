f73b8d95b127f1e00471bf3b3e39c14c2811fe6a
Ryuhikari <fernandovillab@gmail.com>
2017-04-04 12:46:37 +0200

    Enhance automatic and manual drop

--- js/tetris.js
+++ js/tetris.js
@@ -29,6 +29,11 @@ function drawMatrix(matrix, offset) {
 	});
 }
 
+function playerDrop() {
+	player.pos.y++;
+	dropCounter = 0;
+}
+
 let dropCounter = 0;
 let dropInterval = 1000;
 
@@ -39,8 +44,7 @@ function update(time = 0) {
 
 	dropCounter += deltaTime;
 	if (dropCounter > dropInterval) {
-		player.pos.y++;
-		dropCounter = 0;
+		playerDrop();
 	}
 
 	draw();
@@ -58,8 +62,7 @@ document.addEventListener('keydown', event => {
 	} else if (event.keyCode === 39) {
 		player.pos.x++;
 	} else if (event.keyCode === 40) {
-		player.pos.y++;
-		dropCounter = 0;
+		playerDrop();
 	}
 });
 
